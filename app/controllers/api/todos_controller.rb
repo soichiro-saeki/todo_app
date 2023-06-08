18b7639eb1eafdd5400c530f990f5674402c3849
soichiro-saeki <goku.sou19941126@gmail.com>
2023-06-08 10:29:38 +0900

    API実装

--- app/controllers/api/todos_controller.rb
+++ app/controllers/api/todos_controller.rb
@@ -4,4 +4,20 @@ class Api::TodosController < ApplicationController
     @todos = Todo.all
     render json: @todos
   end
+
+  def show
+  @todo = Todo.find_by(id: params[:id])
+  if @todo
+    render json: @todo
+  else
+    render json: { error: 'Todo not found' }, status: :not_found
+  end
+  end
+
+
+  def destroy
+    @todo = Todo.find(params[:id])
+    @todo.destroy
+    head :no_content
+  end
 end
